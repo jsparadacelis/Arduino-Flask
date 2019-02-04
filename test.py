bbce077c8af976621a5abd85645e757306ccec75
jsparadacelis <parada.celis@gmail.com>
2019-02-03 20:09:12 -0500

    Fix write in csv file

--- test.py
+++ test.py
@@ -1,19 +1,25 @@
-import serial
-import requests
-import json
+import serial, requests, json
 
+
+URL_API = "http://localhost:12345/write_csv"
+SERIAL_PORT = "/dev/ttyUSB0"
 #lee los datos del puerto serial
-arduino = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=10.0)
+arduino_data = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=10.0)
 
 
 while True:
     #lee las lineas de la salida del arduino
-    line = arduino.readline()
-    #decodifica la salida en UTF-8
-    print(line.decode('utf-8'))
-    #Armar un json de ejemplo
-    payload = {
-        "temp": line.decode('utf-8')
+    arduino_scope = arduino_data.readline()
+    decoded_bytes = float(arduino_scope[0:len(line)-2].decode("utf-8"))
+    data_to_request = {
+        "temp": decoded_bytes
     }
     #Hace el request al api
-    r = requests.post("http://127.0.0.1:12345/write_csv", json=payload)
+    try:
+        response = requests.post(
+            URL_API, 
+            json=data_to_request
+        )
+    except requests.exceptions.RequestException as error:  
+        print(error)
+        sys.exit(1)
